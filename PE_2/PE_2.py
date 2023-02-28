76040678438950076bc372b5b88099b6a76c43c0
root <root@DESKTOP-L2SFURG.localdomain>
2023-02-28 16:32:27 -0500

    Adding PE_19 & completed PE_2

--- PE_2/PE_2.py
+++ PE_2/PE_2.py
@@ -1,16 +1,28 @@
-def pe2(limit:int) -> int:
 
+
+def pe2(limit:int) -> int:
+    print(limit)
+    print(type(limit))
     term_a=1
     term_b=2
     sum = 0
-    for (term_a < limit) & (term_b < limit):
+    while (term_a < limit) & (term_b < limit):
         if term_a % 2 == 0:
             sum += term_a
         term_a = term_a + term_b
         if term_b % 2 == 0:
             sum += term_b
         term_b = term_a + term_b
-
-
-
     return sum
+
+def optomizedPE2(limit:int) -> int:
+    pass
+    return sum
+
+
+
+if __name__ == "__main__":
+    solution = pe2(limit=4_000_000)
+    print(solution)
+    #solution_optimized = optomizedPE2()
+    #print(solution_optimized)
